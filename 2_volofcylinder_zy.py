1a946631361ff15e0e0bf99fe5cbd4e2da38a088
Zheng Yi <35279486+DHSzhengyi@users.noreply.github.com>
2018-02-04 22:58:55 +0800

    Delete 2_volofcylinder_zy.py

--- 2_volofcylinder_zy.py
+++
@@ -1,8 +0,0 @@
-radius = float(input("Enter radius of the cylinder:"))
-length = float(input("Enter length of the cylinder:"))
-
-pi = 3.1415926535897932384626433832795028841971693993751
-area = radius*radius*pi
-volume = area*length
-
-print("The volume of the cylinder is {0:.2f} cubic metres.".format(volume))
